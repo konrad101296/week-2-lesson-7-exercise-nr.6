218e25cb70f4a32911d2c656f024e3e3934b39cf
konrad101296 <konradslimak@o2.pl>
2024-02-29 19:39:17 +0100

    Add project files.

---
+++ week 2 lesson 7 exercise nr.6/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("podaj swoj wzrost:");
+double wzrost = double.Parse(Console.ReadLine());
+
+if (wzrost <= 152.9)
+{
+    Console.WriteLine("jestes karłowaty");
+}
+else if (wzrost > 152.9 && wzrost <= 162.9)
+{
+    Console.WriteLine("jestes niski");
+}
+else if (wzrost > 162.9 && wzrost <= 172)
+{
+    Console.WriteLine("jestes sredniego wzrostu");
+}
+else if (wzrost > 172 && wzrost <= 177.9)
+{
+    Console.WriteLine("jestes wysoki");
+}
+else if (wzrost > 177.9)
+{
+    Console.WriteLine("jestes bardzo wysoki");
+}
